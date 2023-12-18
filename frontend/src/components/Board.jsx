5067dbf6df4c0a7ecd18ceca40c20f73429e37b5
unknown <paulpleela@gmail.com>
2023-12-18 17:13:47 +0700

    Basic backend setup

---
+++ frontend/src/components/Board.jsx
@@ -0,0 +1,15 @@
+import List from "./List";
+
+export default function Board() {
+  return (
+    <div className="flex max-h-[calc(100vh-4rem)] overflow-x-auto no-scrollbar xl:w-3/4">
+      <List />
+      <List />
+      <List />
+      <List />
+      <div className="flex items-center justify-center text-4xl text-center font-bold text-stone-600 bg-stone-200 shadow-lg h-auto w-14 shrink-0 ml-4 my-7 px-2 rounded-lg overflow-auto no-scrollbar mr-2">
+        +
+      </div>
+    </div>
+  );
+}
